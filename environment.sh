1fb12f61cf774c724a9045f0cc26d44e78ed6aa7
Adam Thornton <ademus4@gmail.com>
2019-01-24 18:46:06 +0000

    Set-up environment

---
+++ environment.sh
@@ -0,0 +1,7 @@
+export DISPLAY=localhost:0.0
+export ROOTSYS=/usr/local/bin/root
+
+# set haspect environment
+export HSCODE=/work/HASPECT6
+export HSEXP=$HSCODE/hsexperiments/clastools
+export CLAS12TOOL=/work/Clas12Tool/
